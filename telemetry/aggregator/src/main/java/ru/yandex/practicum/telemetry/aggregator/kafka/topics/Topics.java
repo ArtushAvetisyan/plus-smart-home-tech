e8f806949b232bab1b4d6d255ebb890216086a7a
Artush Avetisyan <Artush.Avetis@yandex.com>
2026-06-25 15:50:15 +0300

    Feat: Implement aggregator module for sensor event snapshots

--- telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/kafka/topics/Topics.java
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/telemetry/aggregator/kafka/topics/Topics.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.telemetry.collector.kafka.topics;
+package ru.yandex.practicum.telemetry.aggregator.kafka.topics;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,6 +11,6 @@ public class Topics {
     @Value("${app.kafka.topics.sensor}")
     private String sensorTopic;
 
-    @Value("${app.kafka.topics.hub}")
-    private String hubTopic;
+    @Value("${app.kafka.topics.snapshot}")
+    private String snapshotTopic;
 }
