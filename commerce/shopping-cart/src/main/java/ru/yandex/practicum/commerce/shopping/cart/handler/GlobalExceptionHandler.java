7f4f6c811feee46689f8380a3a48cc5cc904e4a9
Artush Avetisyan <Artush.Avetis@yandex.com>
2026-07-05 14:42:53 +0300

    feat(commerce): configure resilience4j circuit breaker for shopping-cart

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/commerce/shopping/cart/handler/GlobalExceptionHandler.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/commerce/shopping/cart/handler/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package ru.yandex.practicum.commerce.shopping.cart.handler;
 
+import feign.FeignException;
+import io.github.resilience4j.circuitbreaker.CallNotPermittedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -38,6 +40,26 @@ public class GlobalExceptionHandler {
                 .message(exception.getMessage())
                 .userMessage(exception.getUserMessage())
                 .httpStatus(HttpStatus.UNAUTHORIZED.name())
+                .build();
+    }
+
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.SERVICE_UNAVAILABLE)
+    public ErrorResponse handleCircuitBreakerOpen(CallNotPermittedException exception) {
+        return ErrorResponse.builder()
+                .message(exception.getMessage())
+                .userMessage("Сервис склада временно недоступен из-за частых сбоев. Пожалуйста, попробуйте оформить заказ позже.")
+                .httpStatus(HttpStatus.SERVICE_UNAVAILABLE.name())
+                .build();
+    }
+
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.BAD_GATEWAY)
+    public ErrorResponse handleFeignException(FeignException exception) {
+        return ErrorResponse.builder()
+                .message(exception.getMessage())
+                .userMessage("Не удалось связаться с сервисом склада. Проверьте параметры заказа или повторите попытку.")
+                .httpStatus(HttpStatus.BAD_GATEWAY.name())
                 .build();
     }
 }
